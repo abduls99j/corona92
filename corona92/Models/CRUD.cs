6e901cf17641d405060152420e69c91831dd6a3e
abduls99j <l174237@lhr.nu.edu.pk>
2020-03-17 22:45:46 +0500

    added CRUD futher in progress

---
+++ corona92/Models/CRUD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corona92.Models
+{
+    public class CRUD
+    {
+        public static string connectionString = "data source=mDESKTOP-O209U5U;Database=covid92;User Id=sa;Password=12345678;";
+
+        public List<covidCase> getCases()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd;
+
+            try
+            {
+                cmd = new SqlCommand("SELECT * FROM covidCases", con);
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+
+                List<covidCase> list = new List<covidCase>();
+                while (rdr.Read())
+                {
+                    covidCase case=
+
+                    case. = rdr["userName"].ToString();
+                    user.password = rdr["userPassword"].ToString();
+                    user.gender = rdr["gender"].ToString();
+                    user.dateOfBirth = rdr["dateOfBirth"].ToString();
+                    user.userEmail = rdr["userEmail"].ToString();
+                    user.Fname = rdr["Fname"].ToString();
+                    user.Lname = rdr["Lname"].ToString();
+                    user.userStatus = rdr["userStatus"].ToString();
+                    list.Add(user);
+                }
+                rdr.Close();
+                con.Close();
+
+                return list;
+            }
+
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error" + ex.Message.ToString());
+                return null;
+
+            }
+        }
+}
--- corona92/Models/covidCase.cs
+++ corona92/Models/covidCase.cs
@@ -7,12 +7,18 @@ namespace corona92.Models
 {
     public class covidCase
     {
-        String province;
-        String city;
-        float longitude, latitude;
-        int confirmed, deaths, recovered;
+        String province { get; set; }
+        String city { get; set; }
+        float longitude { get; set; }
+        float latitude { get; set; }
+        int confirmed { get; set; }
+        int  deaths { get; set; }
+        int recovered { get; set; }
+        public covidCase()
+        {
 
-        covidCase(String _province, String _city, float _longitude, float _latitude,
+        }
+        public covidCase(String _province, String _city, float _longitude, float _latitude,
             int _confirmed,int _deaths,int _recoverd)
         {
             province = _province;
