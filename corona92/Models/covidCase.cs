ee47200645351aafd1880449c59986c221da6013
abduls99j <l174237@lhr.nu.edu.pk>
2020-03-17 19:45:14 +0500

    added covidCase class

---
+++ corona92/Models/covidCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corona92.Models
+{
+    public class covidCase
+    {
+        String province;
+        String city;
+        float longitude, latitude;
+        int confirmed, deaths, recovered;
+
+        covidCase(String _province, String _city, float _longitude, float _latitude,
+            int _confirmed,int _deaths,int _recoverd)
+        {
+            province = _province;
+            city = _city;
+            longitude = _longitude;
+            latitude = _latitude;
+            confirmed = _confirmed;
+            deaths = _deaths;
+            recovered = _recoverd;
+        }
+    }
+}
